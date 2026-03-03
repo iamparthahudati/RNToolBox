ca5d689a5feedb4064ed11f63ed1ee3ac63bde26
Partha Hudati <partha.hudati@transunion.com>
2026-03-03 11:53:54 +0530

    removed firebase

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
-import { setBackgroundHandler } from './src/services/notifications';
+
 const App = () => {
-  useEffect(() => {
-    setBackgroundHandler();
-  }, []);
   return <AppNavigator />;
 };
-
-
-
 
 export default App;
